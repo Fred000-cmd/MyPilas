7fbe2d3b9a6f55a6368d4c7d003e06685ac0cf88
Fredson Oliveira <fredsonoliveira000@gmail.com>
2022-01-31 15:03:24 -0300

    projeto finalizado

--- source.js
+++ source.js
@@ -1,4 +1,14 @@
 const locationLogo = document.querySelector('header img');
 locationLogo.addEventListener('click', ()=>{
   window.location.assign('./index.html');
-})
+});
+
+const collapse = document.querySelector('.collapse');
+let navbar = document.querySelector('.navbar');
+collapse.addEventListener('click', ()=>{ 
+  if(navbar.getAttribute('style')){
+    navbar.removeAttribute('style');
+  }else{
+    navbar.setAttribute('style', 'display: block');
+  }
+});
